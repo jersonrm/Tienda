4f8b9d4360f3d0be2e5053c251f6128e7ddbae4c
jersonr87 <ing.jerson@gmail.com>
2024-11-01 22:08:05 -0500

    Se completa el backend

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,53 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors'; // Importar cors
-
+import cookieParser from 'cookie-parser';
 dotenv.config();
-import products from './data/products.js';
+import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
-// Usar CORS para permitir solicitudes desde otros orígenes
-app.use(cors()); // Permite todas las solicitudes
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-// O especificar el dominio de tu frontend
-// app.use(cors({ origin: 'http://localhost:3000' }));
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
-app.get('/', (req, res) => {
-  res.send('el API está funcionando');
-});
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
-app.listen(port, () => {
-  console.log(`El servidor está corriendo en el puerto ${port}`);
-});
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
