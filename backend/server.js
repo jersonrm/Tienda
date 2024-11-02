bfe0acf701960aec6c315aa57166e715ab684b00
jersonr87 <ing.jerson@gmail.com>
2024-11-01 19:03:51 -0500

    se avanza en el backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,24 @@
-console.log('Hola Mundo');
+import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import products from './data/products.js';
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('el API está funcionando');
+});
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find((p) => p._id === req.params.id);
+    res.json(product);
+});
+
+app.listen(port, () => {
+  console.log(`El servidor está corriendo en el puerto ${port}`);
+});
