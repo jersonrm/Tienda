ce52578b811537f87bbe7912c38045dc8a7dc750
jersonr87 <ing.jerson@gmail.com>
2024-11-01 22:25:49 -0500

    Se finaliza el front]

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
-import { Container } from 'react-bootstrap'
-import { Outlet } from 'react-router-dom'
-import React from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { logout } from './slices/authSlice';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const expirationTime = localStorage.getItem('expirationTime');
+    if (expirationTime) {
+      const currentTime = new Date().getTime();
+
+      if (currentTime > expirationTime) {
+        dispatch(logout());
+      }
+    }
+  }, [dispatch]);
+
   return (
     <>
+      <ToastContainer />
       <Header />
       <main className='py-3'>
         <Container>
@@ -16,7 +34,7 @@ const App = () => {
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
